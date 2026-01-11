e8f0d09eec215764cb7c83266e9ff2a6d20cdd67
gabrielqueeniegomes <queeniegomes93@gmail.com>
2026-01-11 14:18:36 -0500

    looking for pop-up to close

--- src/main/java/com/generic/RegistrationCountry.java
+++ src/main/java/com/generic/RegistrationCountry.java
@@ -32,24 +32,59 @@ driver.findElement(By.xpath("//a[contains(text(), 'REGISTER')]")).click();
         
         Thread.sleep(2000);
         
+        if (closeButton.size() > 0) {
+            System.out.println("Popup found. Closing...");
+            closeButton.get(0).click();
+        }
+        
 WebElement countryDropdown = driver.findElement(By.xpath("//select[contains(@name, 'country')]"));
         
         Select select = new Select(countryDropdown);
         
+        if (closeButton.size() > 0) {
+            System.out.println("Popup found. Closing...");
+            closeButton.get(0).click();
+        }
+        
         select.selectByVisibleText("BANGLADESH");
         Thread.sleep(1000);
+        
+        if (closeButton.size() > 0) {
+            System.out.println("Popup found. Closing...");
+            closeButton.get(0).click();
+        }
         
         select.selectByIndex(12);
         Thread.sleep(1000);
         
+        if (closeButton.size() > 0) {
+            System.out.println("Popup found. Closing...");
+            closeButton.get(0).click();
+        }
+        
         select.selectByValue("CHINA");
         Thread.sleep(1000);
+        
+        if (closeButton.size() > 0) {
+            System.out.println("Popup found. Closing...");
+            closeButton.get(0).click();
+        }
 		
         List<WebElement> dropDownOptions = select.getOptions();
         int optionsCount = dropDownOptions.size();
+        
+        if (closeButton.size() > 0) {
+            System.out.println("Popup found. Closing...");
+            closeButton.get(0).click();
+        }
 		
 		for (int i = 0; i < optionsCount; i++) {
 			String value = dropDownOptions.get(i).getText();
+			
+			if (closeButton.size() > 0) {
+	            System.out.println("Popup found. Closing...");
+	            closeButton.get(0).click();
+	        }
 		
 			if (value.equals("UNITED STATES")) {
                 select.selectByIndex(i);
@@ -58,6 +93,11 @@ WebElement countryDropdown = driver.findElement(By.xpath("//select[contains(@nam
             }
 		}
 
+		if (closeButton.size() > 0) {
+            System.out.println("Popup found. Closing...");
+            closeButton.get(0).click();
+        }
+		
 		Thread.sleep(3000);
 		driver.quit();
 		
